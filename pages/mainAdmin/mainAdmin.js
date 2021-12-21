47a64a5809cf90d3dd862a927f795c5a489756fd
bart7711 <bart0594@stud.kea.dk>
2021-12-21 03:54:39 +0100

    fixed bugs

--- pages/mainAdmin/mainAdmin.js
+++ pages/mainAdmin/mainAdmin.js
@@ -1,7 +1,11 @@
 import renderNavBar from "/pages/navbar/navbar.js";
+let apiKey="";
+let userJWTToken = "";
 
 export default () => {
+    userJWTToken =  JSON.parse(localStorage.getItem("user"));
     const content = document.querySelector(".content");
+    apiKey= `${window.apiUrl}api/`;
   
     fetch("./pages/mainAdmin/mainAdmin.html")
       .then((response) => response.text())
@@ -12,6 +16,99 @@ export default () => {
       });
   };
   
-  function handleAdminPage(){
-    
+async function handleAdminPage(){
+  const tableContainer = document.getElementById("tableContainer");
+  let candidates = await getCandidates();
+  handleCandidates(tableContainer, candidates);
+}
+
+function getCandidates(){
+  const key = apiKey+"candidate";
+  const candidates = fetch(key, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+    });
+  return candidates;
+}
+function handleCandidates(tableCon, candidates){
+  try{
+    candidates = groupBy(candidates,"party");
+    generateTables(tableCon,candidates);
+  }catch(err){}
+}
+
+function groupBy(arr, party) {
+  //Really cool function for splitiing an array into object containing subarrays based
+  //on a property of the object. In this case, party name.
+  return arr.reduce(function(memo, x) {
+    if (!memo[x[party].name]) { memo[x[party].name] = []; }
+    memo[x[party].name].push(x);
+    return memo;
+  }, {});
+}
+function generateTables(tableCon, candidates){
+  for(let key in candidates){
+    const candidateCon = candidates[key];
+    const partyDescription = document.createElement("h3");
+    const party = candidateCon[0].party;
+    partyDescription.textContent=party.name+" - "+party.symbol;
+    tableCon.appendChild(partyDescription);
+    let table = generateTable();
+    candidateCon.forEach(candidate =>generateRows(candidate,table))
+    tableCon.appendChild(table);
+    const br = document.createElement("br")
+    tableCon.appendChild(br);
   }
+}
+  function generateTable(){
+    const table = document.createElement("table");
+    const firstRow = table.insertRow();
+
+    const cellName = firstRow.insertCell();
+    const text0 = document.createTextNode("Full Name");
+    cellName.appendChild(text0);
+
+    const cellVote = firstRow.insertCell();
+    const text1 = document.createTextNode("Vote");
+    cellVote.appendChild(text1);
+
+    return table;
+  }
+
+  function generateRows(candidate,table){
+    const row = table.insertRow();
+    const cell = row.insertCell();
+    const text = document.createTextNode(candidate.fullName);
+    cell.appendChild(text);
+    const buttonCell = row.insertCell();
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add('btn','btn-primary');
+    deleteButton.setAttribute("role","button");
+    deleteButton.innerHTML="Delete";
+    deleteButton.addEventListener("click",()=> deleteCandidate(candidate.id))
+    buttonCell.appendChild(deleteButton);
+  }
+  function deleteCandidate(id){
+    let key = apiKey + "candidate/"+id;
+    fetch(key, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: "Bearer " + userJWTToken.accessToken,
+      },
+    })
+      .then(() => {
+        window.router.navigate("/admin");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
